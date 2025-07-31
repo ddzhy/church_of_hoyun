500ef4fcbdf91f867fdf51254e0b90773c5596b8
ddzhy <hna920037@gmail.com>
2025-07-31 21:59:12 +0900

    members

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,7 +8,7 @@ import image3 from '../assets/image3.jpg';
 const Home = () => {
   return (
     <div className="text-center mt-10">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">호윤교에 오신 것을 환영합니다!</h1>
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">호윤교에 오신 것을 환영합니다.</h1>
       {/* <img src={logo} alt="로고" className="mx-auto mb-6 w-40" /> */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         <img src={image1} alt="대표사진1" className="rounded shadow w-72 mx-auto" />
